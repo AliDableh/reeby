5ee261489dcb0d245ec726a0daf6e9f9c762562a
AliDableh <aliyasen113@gmail.com>
2023-08-02 09:27:05 +0300

    no

--- lib/card.dart
+++ lib/card.dart
@@ -13,14 +13,15 @@ class _aaaState extends State<aaa> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:  AppBar(title: const Text('my card'),),
+      backgroundColor: Color.fromARGB(255, 3, 2, 85),
+      appBar:  AppBar(title: const Text('my card'),backgroundColor: Color.fromARGB(255, 0, 0, 0),),
       body: StreamBuilder<Object>(
         stream: null,
         builder: (context, snapshot) {
           return Column(
             children: [
               Container(
-                height:350 ,
+                height:150 ,
                 child: const Center(
                     child: CircleAvatar(
                       radius: 100,
@@ -28,68 +29,86 @@ class _aaaState extends State<aaa> {
                     ),
                   ),
               ),
-              Text('Flutter Devloper'),
+              SizedBox(height: 20,),
+              Text('Flutter Devloper',style: TextStyle(color: Colors.white,fontSize: 16, ),),
               Divider(
                 color:  Colors.black,
 
               ),
                  Card(
                   color: Colors.white,
-                  child: Row(
-                    children:const [
-                      Icon(
-                        Icons.person
-                      ),
-                      SizedBox(width: 20,),
-                      Text("Ali Yaseen Dableh",style: TextStyle(fontSize: 20,color: Colors.black,),textAlign: TextAlign.center,)
-                      
-                    ],
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      children:const [
+                        Icon(
+                          Icons.person
+                        ),
+                        SizedBox(width: 20,),
+                        Text("Ali Yaseen Dableh",style: TextStyle(fontSize: 20,color: Colors.black,),textAlign: TextAlign.center,)
+                        
+                      ],
+                    ),
                   ),
                 ),
-                SizedBox(height: 35,),
-                Card(
-                  color: Colors.white,
-                  child: Row(
-                    children:const [
-                      Icon(
+                SizedBox(height: 15,),
+                  Card(
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        children:const [
+                          Icon(
 
-                        Icons.email
+                            Icons.email
+                          ),
+                          SizedBox(width: 20,),
+                          Text("aliyasen@gmail.com",style: TextStyle(fontSize: 20,color: Colors.black,),textAlign: TextAlign.center,)
+                          
+                        ],
                       ),
-                      SizedBox(width: 20,),
-                      Text("aliyasen@gmail.com",style: TextStyle(fontSize: 20,color: Colors.black,),textAlign: TextAlign.center,)
-                      
-                    ],
+                    ),
                   ),
-                ),
-                 SizedBox(height: 35,),
+               
+                 SizedBox(height: 15,),
+                   Card(
+                    color: Colors.white,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        children:const [
+                          Icon(
+                            Icons.location_on
+                          ),
+                          SizedBox(width: 20,),
+                          Text("syria",style: TextStyle(fontSize: 20,color: Colors.black,),textAlign: TextAlign.center,)
+                          
+                        ],
+                      ),
+                    ),
+                  ),
+                
+                 SizedBox(height: 15,),
                 Card(
                   color: Colors.white,
-                  child: Row(
-                    children:const [
-                      Icon(
-                        Icons.location_on
-                      ),
-                      SizedBox(width: 20,),
-                      Text("syria",style: TextStyle(fontSize: 20,color: Colors.black,),textAlign: TextAlign.center,)
-                      
-                    ],
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      children:const [
+                        Icon(
+                          Icons.phone
+                        ),
+                        SizedBox(width: 20,),
+                        Text("0992182946",style: TextStyle(fontSize: 20,color: Colors.black,),textAlign: TextAlign.center,)
+                        
+                      ],
+                    ),
                   ),
                 ),
-                 SizedBox(height: 35,),
-                Card(
-                  color: Colors.white,
-                  child: Row(
-                    children:const [
-                      Icon(
-                        Icons.phone
-                      ),
-                      SizedBox(width: 20,),
-                      Text("0992182946",style: TextStyle(fontSize: 20,color: Colors.black,),textAlign: TextAlign.center,)
-                      
-                    ],
-                  ),
-                ),
-              
+              Divider(
+                color: Colors.black,
+                
+              )
             ],
           );
         }
